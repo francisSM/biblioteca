0d593a9bd13f81b28fc7277ae62f10336d276e38
francis <fbisanhueza@gmail.com>
2024-05-04 23:04:50 -0400

    Cambio en los atributos

--- src/main/java/Libro.java
+++ src/main/java/Libro.java
@@ -6,32 +6,16 @@ public class Libro {
     private String autor;
     private String categoria;
     private int ejemplaresDisponibles;
-    private List<Integer> calificaciones;
-    private List<String> comentarios;
 
     public Libro(String titulo, String autor, String categoria, int ejemplaresDisponibles) {
         this.titulo = titulo;
         this.autor = autor;
         this.categoria = categoria;
         this.ejemplaresDisponibles = ejemplaresDisponibles;
-        this.calificaciones = new ArrayList<>();
-        this.comentarios = new ArrayList<>();
-    }
-
-    public void agregarCalificacion(int calificacion) {
-        calificaciones.add(calificacion);
-    }
-
-    public void agregarComentario(String comentario) {
-        comentarios.add(comentario);
     }
 
     public String getTitulo() {
         return titulo;
-    }
-
-    public String getAutor() {
-        return autor;
     }
 
     public String getCategoria() {
@@ -40,18 +24,6 @@ public class Libro {
 
     public int getEjemplaresDisponibles() {
         return ejemplaresDisponibles;
-    }
-
-    public List<Integer> getCalificaciones() {
-        return calificaciones;
-    }
-
-    public List<String> getComentarios() {
-        return comentarios;
-    }
-
-    public void setEjemplaresDisponibles(int ejemplaresDisponibles) {
-        this.ejemplaresDisponibles = ejemplaresDisponibles;
     }
 
     public void prestamoRealizado() {
@@ -71,8 +43,6 @@ public class Libro {
                 ", autor='" + autor + '\'' +
                 ", categoria='" + categoria + '\'' +
                 ", ejemplaresDisponibles=" + ejemplaresDisponibles +
-                ", calificaciones=" + calificaciones +
-                ", comentarios=" + comentarios +
                 '}';
     }
 }
